b93c915c26c7afcde94143e4d140e8e7ef859929
CiprianDumitriu <dumitriuciprian@gmail.com>
2025-07-14 22:24:04 +0300

    Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
 import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Home = () => {
   return (
